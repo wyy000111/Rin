fe096eea96e6c06a6afb3deb5caf1b80292b27c8
Yongyong Wu <104568477+wyy000111@users.noreply.github.com>
2026-02-20 02:51:01 +0800

    Update footer.tsx

--- client/src/components/footer.tsx
+++ client/src/components/footer.tsx
@@ -53,7 +53,7 @@ function Footer() {
  <p>
 <iframe src="https://comment.19781126.xyz/embed/area/www?theme=light&lang=zh-CN"  
 name="评论" 
-width="80%"
+width="800%"
 height="auto"
 frameBorder="0"
 scrolling="yes">
